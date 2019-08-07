b469fdc853cb9a84b8ad854f32899d0d52496197
Dy <gdy1218@gmail.com>
2019-08-07 13:25:55 +0200

    created reducer and actions, moved user state repo state to context

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -7,54 +7,11 @@ import User from './components/users/User';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
-import axios from 'axios';
+import GithubState from './components/context/github/GithubState';
 import './App.css';
 
 const App = () => {
-  const [users, setUsers] = useState([]);
-  const [user, setUser] = useState({});
-  const [repos, setRepos] = useState([]);
-  const [loading, setLoading] = useState(false);
   const [alert, setAlert] = useState(null);
-
-  //search github users
-  const searchUsers = async text => {
-    setLoading(true);
-    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=$
-    {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
-    {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    setUsers(res.data.items);
-    setLoading(false);
-  };
-
-  //get single Github user
-  const getUser = async (username) => {
-    setLoading(true);
-
-    const res = await axios.get(`https://api.github.com/users/${username}?client_id=$
-    {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
-    {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    setUser(res.data);
-    setLoading(false);
-  };
-
-  //Get users repos
-  const getUserRepos = async (username) => {
-    setLoading(true);
-
-    const res = await axios.get(
-      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=$
-    {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
-    {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    setRepos(res.data);
-    setLoading(false)
-  };
-
-  //clear users from state
-  const clearUsers = () => {
-    setUsers([]);
-    setLoading(false);
-  };
 
   //set Alert
   const showAlert = (msg, type) => {
@@ -64,6 +21,7 @@ const App = () => {
   };
 
     return (
+      <GithubState>
       <Router>
       <div className="App">
         <Navbar/>
@@ -73,28 +31,19 @@ const App = () => {
             <Route exact path='/' render={props => (
               <Fragment>
                 <Search 
-                  searchUsers={searchUsers} 
-                  clearUsers={clearUsers} 
-                  showClear={users.length > 0 ? true : false}
                   setAlert={showAlert}
                 />
-                <Users loading={loading} users={users}/>
+                <Users />
               </Fragment>
             )} />
             <Route exact path='/about' component={About} />
-            <Route exact path='/user/:login' render={props => (
-              <User 
-                { ...props } 
-                getUser={getUser} 
-                getUserRepos={getUserRepos} 
-                user={user}
-                repos={repos} 
-                loading={loading}/>
+            <Route exact path='/user/:login' component={User}/>
             )} />
           </Switch>
         </div> 
       </div>
       </Router>
+      </GithubState>
     );
   } 
 
