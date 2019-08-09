6b813c7b6b4d487451dcb9b9304c2f0a5ecd8a7c
Dy <gdy1218@gmail.com>
2019-08-09 18:02:07 +0200

    set up Express server, Backend users, contacts and jwt Autentication

--- contact-keeper/server.js
+++ contact-keeper/server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const connectDB = require('./config/db');
+
 
 const app = express();
+
+//connect Database
+connectDB();
+
+//Init Middleware
+app.use(express.json({ extended: false }));
 
 //app.get('/', (req, res) => res.send('Hello World'));
 app.get('/', (req, res) => 
