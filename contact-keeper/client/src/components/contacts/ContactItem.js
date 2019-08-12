51c6099244c4a8c41437107ed36592c1bce99556
Dy <gdy1218@gmail.com>
2019-08-12 17:41:23 +0200

    finished Contacts UI

--- contact-keeper/client/src/components/contacts/ContactItem.js
+++ contact-keeper/client/src/components/contacts/ContactItem.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
+import ContactContext from '../../context/contact/contactContext';
 
 const ContactItem = ({ contact }) => {
+  const contactContext = useContext(ContactContext);
+  const { deleteContact, setCurrent, clearCurrent } = contactContext;
+
   const { id, name, email, phone, type } = contact;
+
+  const onDelete = () => {
+    deleteContact(id);
+    clearCurrent();
+  }
 
   return (
     <div className="card bg-light">
@@ -26,8 +35,8 @@ const ContactItem = ({ contact }) => {
         </li>)}
       </ul>
       <p>
-        <button className="btn btn-dark btn-sm">Edit</button>
-        <button className="btn btn-danger btn-sm">Delete</button>
+        <button className="btn btn-dark btn-sm" onClick={() => setCurrent(contact)}>Edit</button>
+        <button className="btn btn-danger btn-sm" onClick={onDelete}>Delete</button>
       </p>
     </div>
   )
