f940629246c9551a152c5b9fe3d05fc6787d6505
Dy <gdy1218@gmail.com>
2019-08-13 20:18:22 +0200

    finished logout and navbar and privateRoute Component

--- contact-keeper/client/src/context/auth/AuthState.js
+++ contact-keeper/client/src/context/auth/AuthState.js
@@ -68,10 +68,31 @@ const AuthState = props => {
   }
 
   //Login User
-  const login = () => console.log('login');
+  const login = async formData => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+    try {
+      const res = await axios.post('/api/auth', formData, config);
+
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data
+      });
+
+      loadUser();
+    } catch (err) {
+      dispatch({
+        type: LOGIN_FAIL,
+        payload: err.response.data.msg
+      });
+    }
+  }
 
   //Logout
-  const logout = () => console.log('logout');
+  const logout = () => dispatch({ type: LOGOUT });
 
   //Clear Errors
   const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
